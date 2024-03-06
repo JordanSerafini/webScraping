0592cd2b5e7ee8dea28eaa3b768d4ccca1e13491
JordanSerafini <jordan@solution-logique.fr>
2024-03-06 11:34:25 +0100

    working on booking, scrap first page title and describe ok

--- laforet.mjs
+++ laforet.mjs
@@ -1,55 +1,57 @@
 import puppeteer from 'puppeteer';
 
 export default async function fetchBookingInfo(city) {
-  console.log('Lancement du navigateur...');
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  
-  // Écouteur pour les événements de console de la page.
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    console.log('Lancement du navigateur...');
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  console.log('Visite de la page...');
-  await page.goto(`https://www.laforet.com/agence-immobiliere/annecy-parmelan/acheter`);
+    // Écouteur pour les événements de console de la page.
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
-  console.log('Attente des sélecteurs...');
-  await page.waitForSelector('.apartment-card'); // Sélecteur pour les cartes de propriété
+    console.log('Visite de la page...');
+    await page.goto(`https://www.laforet.com/agence-immobiliere/annecy-parmelan/acheter`);
 
-  console.log('Récupération des données...');
-  const results = await page.evaluate(() => {
-    console.log('Recherche des cartes d\'appartement...');
-    const apartmentCards = Array.from(document.querySelectorAll('.apartment-card'));
-    console.log(`Nombre de cartes trouvées: ${apartmentCards.length}`);
+    console.log('Attente des sélecteurs...');
+    await page.waitForSelector('.apartment-card'); // Sélecteur pour les cartes de propriété
 
-    const getTextContent = (element, selector) => {
-      console.log(`Recherche du texte pour le sélecteur: ${selector}`);
-      const target = element.querySelector(selector);
-      const textContent = target ? target.textContent.trim() : null;
-      console.log(`Texte trouvé: ${textContent}`);
-      return textContent;
-    };
+    console.log('Récupération des données...');
+    const results = await page.evaluate(() => {
+        console.log('Recherche des cartes d\'appartement...');
+        const apartmentCards = Array.from(document.querySelectorAll('.apartment-card'));
+        console.log(`Nombre de cartes trouvées: ${apartmentCards.length}`);
 
-    console.log('Mapping des propriétés...');
-    const apartments = apartmentCards.map(card => {
-      const title = getTextContent(card, '.apartment__label'); 
-      const price = getTextContent(card, '.apartment__price'); 
-      const tags = Array.from(card.querySelectorAll('.tag')).map(tag => {
-        console.log(`Tag trouvé: ${tag.textContent.trim()}`);
-        return tag.textContent.trim();
-      }); 
+        const getTextContent = (element, selector) => {
+            console.log(`Recherche du texte pour le sélecteur: ${selector}`);
+            const target = element.querySelector(selector);
+            const textContent = target ? target.textContent.trim() : null;
+            console.log(`Texte trouvé: ${textContent}`);
+            return textContent;
+        };
 
-      return {
-        title,
-        price,
-        tags
-      };
+        console.log('Mapping des propriétés...');
+        const apartments = apartmentCards.map(card => {
+            const label = getTextContent(card, '.apartment__label');
+            const ville = getTextContent(card, '.apartment-content > p'); 
+            const price = getTextContent(card, '.apartment__price');
+            const tags = Array.from(card.querySelectorAll('.tag')).map(tag => {
+                console.log(`Tag trouvé: ${tag.textContent.trim()}`);
+                return tag.textContent.trim();
+            });
+
+            return {
+                label,
+                ville,
+                price,
+                tags
+            };
+        });
+
+        return apartments;
     });
 
-    return apartments;
-  });
+    console.log('Fermeture du navigateur...');
+    await browser.close();
 
-  console.log('Fermeture du navigateur...');
-  await browser.close();
-
-  console.log('Résultats obtenus:', results);
-  return results;
+    console.log('Résultats obtenus:', results);
+    return results;
 }
