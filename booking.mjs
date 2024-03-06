e16b36d7a59ab7c7a526495298b6bb3c86c4ad76
JordanSerafini <jordan@solution-logique.fr>
2024-03-06 10:07:31 +0100

    :sparkles: setup

--- booking.mjs
+++ booking.mjs
@@ -1,30 +1,34 @@
 import puppeteer from 'puppeteer';
 
-(async () => {
+export default async function fetchBookingInfo(city) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   
   // Modifier le User Agent pour éviter d'être détecté comme un bot.
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
-  await page.goto('https://www.booking.com/searchresults.fr.html?ss=annecy');
+  await page.goto(`https://www.booking.com/searchresults.fr.html?ss=${city}`);
 
   // Attendez que les titres des établissements soient visibles
   await page.waitForSelector('[data-testid="title"]');
 
-  // Récupérez les titres des établissements
-  const titles = await page.evaluate(() => {
+  // Récupérez les titres des établissements et leurs descriptions
+  const results = await page.evaluate(() => {
     const titleElements = Array.from(document.querySelectorAll('[data-testid="title"]'));
-    return titleElements.map(el => el.innerText);
-  });
-  console.log(titles);
-
-  const descriptions = await page.evaluate(() => {
     const descriptionElements = Array.from(document.querySelectorAll('[class="abf093bdfe"]'));
-    return descriptionElements.map(el => el.innerText);
+
+    const hotelResults = titleElements.map((titleElement, index) => {
+      return {
+        name: titleElement.innerText.trim(),
+        description: descriptionElements[index] ? descriptionElements[index].innerText.trim() : 'Description non disponible'
+      };
+    });
+
+    return hotelResults;
   });
-  console.log(descriptions);
+  console.log(results);
 
   await browser.close();
-})();
 
+  return results;
+}
--- index.mjs
+++ index.mjs
@@ -1,43 +1,19 @@
-import puppeteer from 'puppeteer';
+import readline from 'readline';
+import fetchBookingInfo from './booking.mjs';
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  await page.setViewport({ width: 1280, height: 720 });
-  await page.goto('https://developer.chrome.com/');
-
-  // Tapez dans le champ de recherche et appuyez sur Entrée
-  await page.type('.devsite-search-field', 'Puppeteer');
-  await page.keyboard.press('Enter');
-
-  // Attendez que les résultats de recherche soient affichés
-  await page.waitForNavigation({ waitUntil: 'networkidle0' });
-
-  // Utilisez un sélecteur plus spécifique pour les résultats de recherche si nécessaire
-  const searchResults = await page.$$('.gsc-thumbnail-inside .gs-title');
-  if (searchResults.length > 0) {
-    // Au lieu de waitForNavigation, écoutez 'load' ou d'autres événements qui indiquent que la page de destination est prête
-    await Promise.all([
-      searchResults[0].click(), // Cliquez sur le lien
-      // Cette fois, nous utilisons waitForResponse ou un événement qui vous convient mieux
-      page.waitForResponse(response => response.url().includes('/blog/')),
-    ]);
-
-    // Vérifiez que nous sommes sur la bonne page après le clic, inspectez l'URL actuelle
-    console.log(`Current URL: ${page.url()}`);
-
-    // Récupérez le titre en utilisant le sélecteur correct
-    const titleSelector = '.article-title'; // Remplacez ceci par le sélecteur correct pour le titre
-    await page.waitForSelector(titleSelector);
-    const articleTitle = await page.evaluate((sel) => document.querySelector(sel).textContent.trim(), titleSelector);
-
-    console.log(`Le titre de l'article est : "${articleTitle}"`);
-
-    await page.screenshot({ path: 'search_result.png' });
-  } else {
-    console.log("Aucun résultat de recherche trouvé.");
+// Posez une question à l'utilisateur et utilisez la réponse.
+rl.question('Veuillez entrer le nom de la ville pour la recherche : ', async (city) => {
+  try {
+    const bookingInfo = await fetchBookingInfo(city);
+    console.log(bookingInfo);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des informations :', error);
   }
 
-  await browser.close();
-})();
+  rl.close(); 
+});
