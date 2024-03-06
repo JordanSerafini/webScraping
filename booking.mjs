6837c4c6f1f39e35779f419b4c8bfde5ee5ef677
JordanSerafini <jordan@solution-logique.fr>
2024-03-06 10:47:35 +0100

    working on booking, scrap first page title and describe ok

--- booking.mjs
+++ booking.mjs
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 export default async function fetchBookingInfo(city) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  
+
   // Modifier le User Agent pour éviter d'être détecté comme un bot.
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
@@ -13,25 +13,34 @@ export default async function fetchBookingInfo(city) {
   await page.waitForSelector('[data-testid="title"]');
 
   // Récupérez les titres des établissements et leurs descriptions
-  const results = await page.evaluate(() => {
+  const results = await page.evaluate(async () => {
     const titleElements = Array.from(document.querySelectorAll('[data-testid="title"]'));
     let descriptionElements = Array.from(document.querySelectorAll('[class="abf093bdfe"]'));
-    
+
     // Supprimez les deux premières descriptions
     descriptionElements = descriptionElements.slice(2);
-  
+
     const hotelResults = titleElements.map((titleElement, index) => {
       return {
         name: titleElement.innerText.trim(),
         // Utilisez l'index correct pour les descriptions après avoir supprimé les deux premiers éléments
         description: descriptionElements[index] ? descriptionElements[index].innerText.trim() : 'Description non disponible'
       };
+
     });
-  
+
     return hotelResults;
   });
-  
-  console.log(results);
+
+    const moreResultsSelector = 'button[aria-label="Afficher plus de résultats"]';
+    const moreResultsButton = await page.$(moreResultsSelector);
+    if (moreResultsButton) {
+        await moreResultsButton.click();
+        await page.waitForTimeout(2000);
+    }
+    
+
+
 
   await browser.close();
 
